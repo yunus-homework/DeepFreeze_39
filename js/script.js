66494ddb7ad04091e55c9933b7d4b910e1f5d938
Yunus <seytnazarov.yunus@gmail.com>
2022-04-16 13:00:18 +0400

    DeepFreeze_Object

--- js/script.js
+++ js/script.js
@@ -39,12 +39,48 @@ function deepFreeze(obj) {
 deepFreeze(user);
 
 //Uncaught TypeError: Cannot add property test, object is not extensible
-user.test = "test";
-console.log(user.test);
+// user.test = "test";
+// console.log(user.test);
 
-//true
+// all true
 console.log(deepFreeze(user));
 console.log(Object.isFrozen(user));
 console.log(Object.isFrozen(user.data));
 console.log(Object.isFrozen(user.data.d));
 console.log(Object.isFrozen(user.data.d.d1));
+
+// Object.preventExtensions(user);
+
+// user.data.a = "12331";
+
+// console.log(user.data.a);
+
+// console.log(user);
+
+// function deepFreeze(obj) {
+//     // Получаем имена свойств из объекта obj
+//     const propNames = Object.getOwnPropertyNames(obj);
+
+//     // Замораживаем свойства для заморозки самого объекта
+//     propNames.forEach(function (name) {
+//         const prop = obj[name];
+
+//         // Заморозка свойства prop, если оно объект
+//         if (typeof prop === "object" && prop !== null) {
+//             deepFreeze(prop);
+//         } else {
+//             const descriptor = Object.getOwnPropertyDescriptor(obj, name);
+//             descriptor.writable = false;
+//             descriptor.configurable = false;
+//             Object.defineProperty(obj, name, descriptor);
+//         }
+//     });
+
+//     return obj;
+// }
+
+// deepFreeze(user);
+
+// Uncaught TypeError: Cannot add property test, object is not extensible
+// user.data = "test";
+// console.log(Object.getOwnPropertyDescriptor(user, "data"));
